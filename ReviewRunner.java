2cec3c78e0fc7dddbb2fc7e4f229aa120bbfac9f
heyscottn <heyscottn@gmail.com>
2024-01-15 08:17:49 -0600

    edits

--- ReviewRunner.java
+++ ReviewRunner.java
@@ -1,7 +1,45 @@
-class ReviewRunner {
-  public static void main(String[] args) 
-  {
-    /* your code here, for example: */
-    System.out.println(Review.sentimentVal("good"));
-  }
-}
+class ReviewRunner{
+    public static void main(String[] args){
+        // step zero
+        // look through the provided files
+        // especially look at the Review class and its methods
+
+
+
+        // activity 1 - call the 'sentimentVal' method several times
+        // (more details on the handout)
+
+
+
+
+        // activity 2a - write the 'totalSentiment' method
+        // Note: this requires creating "SimpleReview.txt"
+        // TIP: be sure to look for any helper methods in the Review class
+        // TIP: indexOf and substring will be your friend here
+
+
+
+
+        // activity 2b - write the 'starRating' method
+        // this is NOT described in the book, here is one possible system
+        // sentiment val      star rating
+        // less than -10      0
+        // less than 0        1
+        // less than 10       2
+        // less than 20       3
+        // 20 or more         4
+
+
+
+
+        // Activity 3 - a 'fakeReview'
+        // TIP 1: create a new text file for this 'SimpleReview2.txt'
+        // you'll also need to populate your 'positiveAdjectives.txt' file with at least 20 positive adjectives
+        // TIP 2: did you notice a great helper method in Review.java? Something about adjectives?
+        // Note: your final string does not require punctuation
+
+
+
+
+    } // close main method
+} // close Main class
